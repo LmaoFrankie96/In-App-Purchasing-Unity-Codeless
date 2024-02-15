6b3072ad9cd44a148a64b472f2bcee3379b49fd3
Muhammad Ramish <mramish9696@gmail.com>
2024-02-15 14:31:12 +0500

    - all products being bought successfully. now adding currency in game

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
 using UnityEngine.UI;
 
 public class StoreManager : MonoBehaviour
@@ -13,9 +14,30 @@ public class StoreManager : MonoBehaviour
     [Header("In App Products")]
     public GameObject coin500;
     public GameObject removeAds;
+
+    [Header("In App Product IDs")]
+    private string _coin500ID = ".com.Adeetheknights._In_App_Testing.coin500";
+    private string _removeAdsID = ".com.Adeetheknights._In_App_Testing.removeads";
     private void Awake()
     {
         consumablePrice = coin500.GetComponent<IAPButton>().priceText;
         nonConsumablePrice = removeAds.GetComponent<IAPButton>().priceText;
+    }
+
+    public void OnPurchaseComplete(Product product) {
+
+        if (product.definition.id == _coin500ID) {
+
+            Debug.Log("500 coins purchased");
+        }
+        if (product.definition.id == _removeAdsID)
+        {
+
+            Debug.Log("Remove ads purchased");
+        }
+    }
+    public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureReason) {
+
+        Debug.Log($"{product.definition.id} purchase failed because of {failureReason}");
     }
 }
