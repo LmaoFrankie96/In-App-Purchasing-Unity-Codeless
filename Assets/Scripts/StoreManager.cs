70d10c459cc4932eb82232e853c46667508917ea
Muhammad Ramish <mramish9696@gmail.com>
2024-02-15 15:24:02 +0500

    - added the functionality of a non-consumable bike

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -14,20 +14,25 @@ public class StoreManager : MonoBehaviour
     public Text nonConsumablePrice;
 
     [Header("In App Products")]
-    public GameObject coin500;
-    public GameObject removeAds;
+    public Button coin500;
+    public Button newBikeBtn;
+    public Text newBikeText;
 
     [Header("In App Product IDs")]
-    private string _coin500ID = ".com.Adeetheknights._In_App_Testing.coin500";
-    private string _removeAdsID = ".com.Adeetheknights._In_App_Testing.removeads";
+    private string _coin500ID = ".com.adeetheknights._In_App_Testing.coin500";
+    private string _newBikeID = ".com.adeetheknights._In_App_Testing.buynewbike";
 
     [Header("Currency Amounts")]
     public Text coinsAmount;
+
+    [Header("Inventory Items")]
+    public Image bikeIcon;
     private void Awake()
     {
         /*consumablePrice = coin500.GetComponent<IAPButton>().priceText;
-        nonConsumablePrice = removeAds.GetComponent<IAPButton>().priceText;*/
+        nonConsumablePrice = newBikeBtn.GetComponent<IAPButton>().priceText;*/
         SetWallet();
+        SetInventory();
     }
 
     public void OnPurchaseComplete(Product product)
@@ -40,10 +45,12 @@ public class StoreManager : MonoBehaviour
             Debug.Log("500 coins purchased");
 
         }
-        if (product.definition.id == _removeAdsID)
+        if (product.definition.id == _newBikeID)
         {
-
-            Debug.Log("Remove ads purchased");
+            PlayerPrefs.SetInt("Bike1", 1);
+            bikeIcon.gameObject.SetActive(true);
+            DisablePurchaseButton(newBikeBtn, newBikeText);
+            Debug.Log("New Bike purchased");
         }
     }
     public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureReason)
@@ -62,5 +69,18 @@ public class StoreManager : MonoBehaviour
 
         _playerCoins = PlayerPrefs.GetInt("Coins", 0);
         coinsAmount.text = _playerCoins.ToString();
+    }
+    private void SetInventory() {
+
+        if (PlayerPrefs.GetInt("Bike1", 0) == 1) {
+
+            bikeIcon.gameObject.SetActive(true);
+            DisablePurchaseButton(newBikeBtn, newBikeText);
+        }
+    }
+    private void DisablePurchaseButton(Button button, Text text) {
+        button.interactable = false;
+        text.text = "Purchased";
+
     }
 }
