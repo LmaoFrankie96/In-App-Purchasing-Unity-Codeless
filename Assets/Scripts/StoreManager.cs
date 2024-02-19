8e96a4c12f4a6a1fdc8c117a5d75cdbf172d7fed
Muhammad Ramish <mramish9696@gmail.com>
2024-02-19 09:57:02 +0500

    - all products prices are being automatically updated according to local server location

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -10,18 +10,18 @@ public class StoreManager : MonoBehaviour
 {
     private int _playerCoins;
     [Header("In App Price Texts")]
-    public Text consumablePrice;
-    public Text nonConsumablePrice;
-    
+    public Text coin500Text;
+    public Text newBikeText;
+
     [Header("In App Products")]
     //public Product coin500;
     //public Product newBike;
     public Button coin500Btn;
     public Button newBikeBtn;
-    public Text newBikeText;
+
 
     [Header("In App Product IDs")]
-    private string _coin500ID = ".com.adeetheknights._In_App_Testing.coin500Btn";
+    private string _coin500ID = ".com.adeetheknights._In_App_Testing.coin500";
     private string _newBikeID = ".com.adeetheknights._In_App_Testing.buynewbike";
 
     [Header("Currency Amounts")]
@@ -33,8 +33,7 @@ public class StoreManager : MonoBehaviour
     {
         SetWallet();
         SetInventory();
-        consumablePrice = coin500Btn.GetComponentInParent<IAPButton>().priceText;
-        //nonConsumablePrice = newBikeBtn.GetComponent<IAPButton>().priceText;
+        
     }
 
     public void OnPurchaseComplete(Product product)
@@ -60,6 +59,28 @@ public class StoreManager : MonoBehaviour
 
         Debug.Log($"{product.definition.id} purchase failed because of {failureReason}");
     }
+    public void OnProductFetched(Product product)
+    {
+        if (product.definition.id == _coin500ID)
+        {
+            if (coin500Text != null)
+            {
+
+                coin500Text.text = product.metadata.localizedPriceString;
+                Debug.Log("Priceof coin is being changed to localized price");
+            }
+        }
+        if (product.definition.id == _newBikeID)
+        {
+            if (newBikeText != null)
+            {
+
+                newBikeText.text = product.metadata.localizedPriceString;
+                Debug.Log("Priceof bike is being changed to localized price");
+                SetInventory();
+            }
+        }
+    }
     private void UpdateWallet()
     {
         _playerCoins += 500;
@@ -72,18 +93,21 @@ public class StoreManager : MonoBehaviour
         _playerCoins = PlayerPrefs.GetInt("Coins", 0);
         coinsAmount.text = _playerCoins.ToString();
     }
-    private void SetInventory() {
+    private void SetInventory()
+    {
 
-        if (PlayerPrefs.GetInt("Bike1", 0) == 1) {
+        if (PlayerPrefs.GetInt("Bike1", 0) == 1)
+        {
 
             bikeIcon.gameObject.SetActive(true);
             DisablePurchaseButton(newBikeBtn, newBikeText);
         }
     }
-    private void DisablePurchaseButton(Button button, Text text) {
+    private void DisablePurchaseButton(Button button, Text text)
+    {
         button.interactable = false;
         text.text = "Purchased";
 
     }
-  
+
 }
